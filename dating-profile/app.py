364d64e3b539b01b89397bc0bedcb48eaeb1eb6c
lennijusten <lenni.justen@gmail.com>
2023-10-12 22:15:31 -0400

    Revert "move css to static"
    This reverts commit 289fe66262aca32db7249e9d847fa4b3e9ab8655.

--- dating-profile/app.py
+++ dating-profile/app.py
@@ -1,20 +1,17 @@
 from flask import Flask, render_template
+import random
+import string
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # Sample data for an alien profile
-    profile = {
-        'image': 'static/img/profile.png',
-        'name': 'Zorgon',
-        'planet': 'Mars',
-        'age': '120',
-        'bio': 'Alien from Mars who loves space travel.',
-        'interests': ['Space exploration', 'Galactic dance', 'Star gazing']
-    }
-    return render_template('index.html', profile=profile)
+def profile():
+    # Generating random name, location, and bio
+    name = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=10))
+    location = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=5))
+    bio = ' '.join(''.join(random.choices(string.ascii_lowercase, k=5)) for _ in range(20))  # 20 words of 5 letters each
+
+    return render_template('profile.html', name=name, location=location, bio=bio)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
